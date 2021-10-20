995d6ab81132f8981e97dce7bdee7425811ab278
vfreed0m <vlad001002003@gmail.com>
2021-10-21 00:57:28 +0300

    first commit

---
+++ contact.php
@@ -0,0 +1,33 @@
+<?php
+$field_name = $_POST['cf_name'];
+$field_email = $_POST['cf_email'];
+$field_message = $_POST['cf_phone'];
+
+$mail_to = 'vfreed0mz01@gmail.com';
+$subject = 'Message from a site visitor '.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Phone: '.$field_message
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for the message. We will contact you shortly.');
+		window.location = 'contact_page.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please, send an email to vfreed0mz01@gmail.com');
+		window.location = 'contact_page.html';
+	</script>
+<?php
+}
+?>
